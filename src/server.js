126b4a5f788b63e19bbf63c9d7e864c56d936d62
pissulin <pissulin.ads@gmail.com>
2021-03-26 14:31:49 -0300

    refatorando codigo para mais legivel

--- src/server.js
+++ src/server.js
@@ -6,6 +6,9 @@ const axios = require('axios')
 const fs = require('fs')
 const path = require('path')
 const https = require('https')
+const express = require('express');
+
+
 
 const cert = fs.readFileSync(
     path.resolve(__dirname,`../certs/${process.env.GN_CERT}`)
@@ -20,20 +23,29 @@ const credentials = Buffer.from(
     `${process.env.GN_CLIENT_ID}:${process.env.GN_CLIENT_SECRET}`
 ).toString('base64')
 
-axios({
-    method: 'POST',
-    url: `${process.env.GN_ENDPOINT}/oauth/token`,
-    headers:{
-        Authorization: `Basic ${credentials}`,
-        'Content-Type': 'application/json'
-    },
-    httpsAgent: agent,
-    data: {
-        grant_type: 'client_credentials'
-    }
-}).then(response => {
-    const accessToken = response.data?.access_token
 
+const app = express();
+
+app.set('view engine', 'ejs');
+app.set('views', 'src/views');
+
+app.get('/', async (req, res) =>{
+
+    const authResponse = await axios({
+        method: 'POST',
+        url: `${process.env.GN_ENDPOINT}/oauth/token`,
+        headers:{
+            Authorization: `Basic ${credentials}`,
+            'Content-Type': 'application/json'
+        },
+        httpsAgent: agent,
+        data: {
+            grant_type: 'client_credentials'
+        }
+    })
+
+    const accessToken = authResponse.data?.access_token
+    
     const reqGN = axios.create({
         baseURL: process.env.GN_ENDPOINT,
         httpsAgent: agent,
@@ -42,7 +54,7 @@ axios({
             'Content-Type': 'application/json'
         }
     })
-
+    
     const dataCob = {
         "calendario": {
             "expiracao": 3600
@@ -53,7 +65,13 @@ axios({
         "chave": "1aeb48dd-0e90-4e52-bd68-26fa5bf965c0",
         "solicitacaoPagador": "Rifa online"
     }
+        
+    const cobResponse = await reqGN.post('/v2/cob',dataCob)
+    
+    res.send(cobResponse.data)
+})
 
 
-    reqGN.post('/v2/cob',dataCob).then(response => console.log(response.data))
+app.listen(8000, ()=>{
+    console.log('Rodando servidor')
 })
