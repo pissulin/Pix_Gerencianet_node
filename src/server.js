167b42150c6b19a25e1e5b10a38b4340b1f806e9
pissulin <pissulin.ads@gmail.com>
2021-03-26 13:31:26 -0300

    axios com config padrao

--- src/server.js
+++ src/server.js
@@ -31,4 +31,29 @@ axios({
     data: {
         grant_type: 'client_credentials'
     }
-}).then(response => console.log(response.data))
+}).then(response => {
+    const accessToken = response.data?.access_token
+
+    const reqGN = axios.create({
+        baseURL: process.env.GN_ENDPOINT,
+        httpsAgent: agent,
+        headers: {
+            Authorization: `Bearer ${accessToken}`,
+            'Content-Type': 'application/json'
+        }
+    })
+
+    const dataCob = {
+        "calendario": {
+            "expiracao": 3600
+        },
+        "valor": {
+            "original": "10.00"
+        },
+        "chave": "1aeb48dd-0e90-4e52-bd68-26fa5bf965c0",
+        "solicitacaoPagador": "Rifa online"
+    }
+
+
+    reqGN.post('/v2/cob',dataCob).then(response => console.log(response.data))
+})
